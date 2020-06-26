42be5773ef57a6183957ffd82a23ce7d263b21c0
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-06-26 18:04:05 +0800

    Added pending for signature

---
+++ src/hooks/useFetchPendingSignature.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { fetchRows } from '../api';
+import { CDV_CHECK_STAT_FSIGNATURE } from '../constants';
+
+function isPending(row) {
+  return Boolean(
+    row.apv_no
+    && row.cdv_no
+    && row.cdv_checkStatus === CDV_CHECK_STAT_FSIGNATURE
+  );
+}
+
+export default function useFetchPendingSignature(sheetId, refresh) {
+  const [ loading, setLoading ] = useState(false);
+  const [ rows, setRows ] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchRows(sheetId)
+      .then(rows => {
+        const pending = rows.filter(isPending);
+        setRows(pending);
+      })
+      .finally(_ => setLoading(false));
+  }, [sheetId]);
+
+  useEffect(() => {
+    if (refresh) {
+      setLoading(true);
+      fetchRows(sheetId)
+        .then(rows => {
+          const pending = rows.filter(isPending);
+          setRows(pending);
+        })
+        .finally(_ => setLoading(false));
+    }
+  }, [refresh]); // eslint-disable-line
+
+  return {
+    rows,
+    loading,
+  };
+}
---
+++ src/pages/PendingSignature/PendingSignatureTable.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import useSheetId from '../../hooks/useSheetId';
+import useFetchPendingSignature from '../../hooks/useFetchPendingSignature';
+import ListTable from '../../components/ListTable';
+import { formatValueByType } from '../../utils';
+import { LABEL_MAP as Label, TYPE_MAP as Type } from '../../constants';
+
+const columns = [
+  { field: "rcp_item", title: Label["rcp_item"] },
+  { field: "rcp_payee", title: Label["rcp_payee"] },
+  { field: "rcp_invoiceRef", title: Label["rcp_invoiceRef"],  },
+  { title: Label["rcp_amtPeso"], type: "numeric", render: r => formatValueByType(r.rcp_amtPeso, Type["rcp_amtPeso"]) },
+  { title: Label["rcp_amtDollar"], type: "numeric", render: r => formatValueByType(r.rcp_amtDollar, Type["rcp_amtDollar"]) },
+  { title: Label["rcp_dateDue"], type: "date", render: r => formatValueByType(r.rcp_dateDue, Type["rcp_dateDue"]) },
+  { field: "apv_no", title: Label["apv_no"] },
+  { title: Label["apv_dateTransaction"], type: "date", render: r => formatValueByType(r.dateTransaction, Type["apvDateTransaction"]) },
+  { field: "cdv_no", title: Label["cdv_no"] },
+];
+
+function PendingSignatureTable(props) {
+  const { refresh, onSelectionChange, onEditClick } = props;
+  const [ sheetId ] = useSheetId();
+  const { rows, loading } = useFetchPendingSignature(sheetId, refresh);
+
+  return (
+    <ListTable
+      title="Pending for Signature"
+      rows={rows}
+      loading={loading}
+      columns={columns}
+      onEditClick={onEditClick}
+      onSelectionChange={onSelectionChange}
+    />
+  );
+}
+
+export default PendingSignatureTable;
